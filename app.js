6db1ff29320e0472851b893d7081914b9f88ad48
Vitalii <vhrushyn@gmail.com>
2019-08-28 19:00:58 +0300

    aa

--- app.js
+++ app.js
@@ -13,3 +13,31 @@ for (let i = 0; i < addToCartButtons.length; i++) {
 		console.log("clicked button # " + i);
 	})
 }
+
+
+let likes = document.querySelectorAll(".product-like");
+console.log(likes);
+
+for (let i = 0; i < likes.length; i++) {
+	likes[i].addEventListener("click", function() {
+
+		// let prevProductCount = +productsCountEl.textContent;
+		// productsCountEl.textContent = prevProductCount + 1;
+
+		let backgroundImageURL = likes[i].style.backgroundImage //= "url(images/like-light.png)";
+		
+		if (backgroundImageURL != 'url("images/like-light.png")') {
+			likes[i].style.backgroundImage = "url(images/like-light.png)";
+			likes[i].style.backgroundColor ="#2c71b8";
+		} else {
+			likes[i].style.backgroundImage = "url(images/like-dark.png)";
+			likes[i].style.backgroundColor = "transparent";
+		}
+
+		console.log("clicked like # " + i);
+		console.log(likes[i]);
+		console.log("1 = " + backgroundImageURL);
+		console.log("2 = " + likes[i].style.backgroundColor);
+	})
+}
+
